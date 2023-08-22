900c3d9bfa861f87b79d9bd679f56d162cc65ef4
guelowrd <gaylord.warner@gmail.com>
2023-08-22 16:14:45 +0100

    complete page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,122 @@
-import Image from 'next/image'
+import {
+  EthereumClient,
+  w3mConnectors,
+  w3mProvider,
+} from "@web3modal/ethereum";
+import { useWeb3Modal, Web3Modal } from "@web3modal/react";
+import SuperfluidWidget, {
+  EventListeners,
+  supportedNetworks,
+  WalletManager,
+} from "@superfluid-finance/widget";
+import superTokenList from "@superfluid-finance/tokenlist";
+import { useState, useMemo } from "react";
+import { configureChains, createConfig, WagmiConfig, useNetwork } from "wagmi";
+
+import paymentDetails from "../components/paymentDetails";
+import productDetails from "../components/productDetails";
+import { apolloClient, query } from "../components/lens";
+
+const projectId = "952483bf7a0f5ace4c40eb53967f1368";
+
+const { publicClient } = configureChains(supportedNetworks, [
+  w3mProvider({ projectId }),
+]);
+
+const wagmiConfig = createConfig({
+  autoConnect: false,
+  connectors: w3mConnectors({
+    projectId,
+    chains: supportedNetworks,
+  }),
+  publicClient,
+});
+const ethereumClient = new EthereumClient(wagmiConfig, supportedNetworks);
 
 export default function Home() {
+  const { open, isOpen } = useWeb3Modal();
+  const walletManager = useMemo<WalletManager>(
+    () => ({
+      open,
+      isOpen,
+    }),
+    [open, isOpen],
+  );
+
+  const [recipient, setRecipient] = useState("");
+  const [recipientIsValid, setRecipientIsValid] = useState("");
+
+  const handleRecipientChange = (e) => {
+    console.log("Set recipient: " + recipient);
+    setRecipient(e.target.value);
+  }
+
+  const handleRecipientCheck = async () => {
+    console.log("Checking recipient...");
+    checkRecipient();
+  }
+
+  const checkRecipient = async () => {
+    const response = await apolloClient.query({
+      query: query,
+      variables: {
+        address: recipient
+      }
+    });
+    setRecipientIsValid(response.data.defaultProfile == null ? "false" : "true");
+    console.log("Recipient is valid: " + recipientIsValid);
+    console.log(response);
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <main className="bg-white min-h-screen flex-col items-center justify-between p-24">
+      <section className="w-full m-4 flex items-center justify-between">
+        <input
+          className="w-2/3 h-12 rounded mr-4 p-2 border-2 border-gray-300 hover:border-gray-500 focus:border-gray-700"
+          type="text"
+          id="recipientInput"
+          name="recipientInput"
+          placeholder="Enter the recipient address"
+          onChange={handleRecipientChange}
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+          className="bg-[#1aa023] font-semibold w-1/3 h-12 ml-4 rounded-3xl text-white"
+          onClick={handleRecipientCheck}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          Check Recipient
+        </button>
+      </section>
+      <section className="w-full m-4">
+        {recipientIsValid == "true" ?
+          <>
+            <WagmiConfig config={wagmiConfig}>
+              <SuperfluidWidget
+                productDetails={productDetails}
+                paymentDetails={paymentDetails}
+                tokenList={superTokenList}
+                type="dialog"
+                walletManager={walletManager}
+              >
+                {({ openModal }) => (
+                  <button
+                    className="bg-[#1aa023] w-full font-semibold h-12 self-auto rounded-3xl text-white"
+                    onClick={() => openModal()}
+                  >Create Stream</button>
+                )}
+              </SuperfluidWidget>
+            </WagmiConfig>
+            <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+          </>
+          : recipientIsValid == "false" ?
+            <h3 className="self-auto">
+              {recipient} is not a valid recipient address (no Lens handle).
+            </h3>
+            :
+            <h3 className="self-auto">
+              Click "Check Recipient" to confirm {recipient} address is valid (i.e. it has a Lens handle).
+            </h3>
+        }
+      </section>
     </main>
   )
 }
